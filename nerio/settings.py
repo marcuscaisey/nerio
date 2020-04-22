bcdf3c8f065e3cd8a224d285e73a4d9f829fec2b
Marcus Caisey <marcus@teckna.com>
2020-04-22 17:43:04 +0100

    Add users app with custom User model

--- nerio/settings.py
+++ nerio/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -114,3 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
+
+
+# Users
+
+AUTH_USER_MODEL = "users.User"
