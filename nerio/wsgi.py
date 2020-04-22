627124f9f5f561fb77ab1048d04b8bb44a481e2c
Marcus Caisey <marcus@teckna.com>
2020-04-22 16:50:39 +0100

    Run pre-commit on existing code

--- nerio/wsgi.py
+++ nerio/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nerio.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nerio.settings")
 
 application = get_wsgi_application()
