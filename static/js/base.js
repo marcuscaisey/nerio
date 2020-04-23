247f67b4e3431b3fb897ed0996ac344fc91d8caa
Marcus Caisey <marcus@teckna.com>
2020-04-23 12:39:40 +0100

    Add base template

---
+++ static/js/base.js
@@ -0,0 +1,19 @@
+/*
+ * Toggle the is-active class on the navbar burger and menu when the navbar
+ * burger is clicked.
+ */
+function setupNavbarBurger() {
+  const navbarBurger = document.querySelector(".navbar-burger");
+  const navbarMenu = document.querySelector(".navbar-menu")
+  navbarBurger.addEventListener("click", () => {
+    navbarBurger.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
+  });
+}
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", setupNavbarBurger);
+} else {
+  setupNavbarBurger();
+}
+
