2d007d616e8514227a09b26364da124df83a3f6f
Marcus Caisey <marcus@teckna.com>
2020-05-07 16:26:39 +0100

    Clean up setupNavbarBurger

--- static/js/base.js
+++ static/js/base.js
@@ -5,6 +5,7 @@
 function setupNavbarBurger() {
   const navbarBurger = document.querySelector(".navbar-burger");
   const navbarMenu = document.querySelector(".navbar-menu");
+
   navbarBurger.addEventListener("click", () => {
     navbarBurger.classList.toggle("is-active");
     navbarMenu.classList.toggle("is-active");
