893af1de9e93c292947a2442ef67508e460ae676
Marcus Caisey <marcus@teckna.com>
2020-06-02 13:49:47 +0100

    Document core views

--- core/views.py
+++ core/views.py
@@ -9,6 +9,18 @@ from .models import URL
 
 
 def home(request):
+    """
+    GET
+        Render the home template with an empty url creation form and either the
+        currently logged in user's urls or the urls stored in the current
+        session.
+
+    POST
+        Process the form data from a submitted url creation form. If the form is
+        valid, redirect back to the home view. If not, then process the request
+        as if it was a GET, replacing the empty url creation form with the
+        current invalid form.
+    """
     if request.method == "POST":
         form = URLCreationForm(request.POST)
         if form.is_valid():
@@ -38,6 +50,11 @@ def home(request):
 
 
 def forward_url(request, name):
+    """
+    Forward the user to the target of the url with the given name and increment
+    its visit count by 1. Return a 404 if a url with the given name doesn't
+    exist.
+    """
     url = get_object_or_404(URL, name=name)
     url.visits = F("visits") + 1
     url.save()
@@ -45,6 +62,38 @@ def forward_url(request, name):
 
 
 def modify(request, pk):
+    """
+    Endpoint to allow renaming and deleting of a user's urls. Valid methods are
+    PATCH and DELETE.
+
+    For all errors apart from 405 Method Not Allowed, the response will be as
+    follows:
+    {
+        "error": "message describing error"
+    }
+
+    For both PATCH and DELETE methods, return a 404 if the requested url doesn't
+    exist and 403 if the requesting user doesn't own the requested url.
+
+    PATCH
+        Rename a user's url with the following request body:
+        {
+            "name": "new name for url"
+        }
+
+        If successful, the response will be as follows:
+        {
+            "name" "new name for url",
+            "url": "new shortened url",
+        }
+
+        If an error occurs when saving the new name, the error message will be
+        returned instead.
+
+    DELETE
+        Delete a user's url. There is no request body required and no body in
+        the response if the deletion is successful.
+    """
     allowed_methods = ("PATCH", "DELETE")
     if request.method not in allowed_methods:
         return HttpResponseNotAllowed(allowed_methods)
