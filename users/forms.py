7ed15d60baf2df4910f4b72c1de2cf1c4886ce8d
Marcus Caisey <marcus@teckna.com>
2020-06-04 17:10:46 +0100

    Add forgot password link as help text on password field

--- users/forms.py
+++ users/forms.py
@@ -1,13 +1,19 @@
 from django.contrib.auth import forms as auth_forms
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 
 class AuthenticationForm(auth_forms.AuthenticationForm):
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.error_messages[
             "invalid_login"
         ] = "The %(username)s and password provided do not match any of our records."
-        super().__init__(*args, **kwargs)
+        self.fields[
+            "password"
+        ].help_text = (
+            f"<a href='{reverse('users:password-reset')}'>Forgot password?</a>"
+        )
 
 
 class UserCreationForm(auth_forms.UserCreationForm):
