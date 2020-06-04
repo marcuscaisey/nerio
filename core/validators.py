e1243ab1f824eff7ab779d35d5e674cbdad0b306
Marcus Caisey <marcus@teckna.com>
2020-06-04 15:29:04 +0100

    Wrap reverse call in try except in case the input doesn't match the url pattern

--- core/validators.py
+++ core/validators.py
@@ -1,6 +1,6 @@
 from django.apps import apps
 from django.core.validators import RegexValidator, ValidationError
-from django.urls import resolve, reverse
+from django.urls import NoReverseMatch, resolve, reverse
 
 
 class URLNameCharacterValidator(RegexValidator):
@@ -23,7 +23,11 @@ def validate_url_name_doesnt_clash(name):
     Validate that a url name won't cause a clash with an existing url when
     forwarding.
     """
-    potential_url = reverse("core:forward", args=(name,))
+    try:
+        potential_url = reverse("core:forward", args=(name,))
+    except NoReverseMatch:
+        return
+
     resolved_view = resolve(potential_url)
     if resolved_view.view_name != "core:forward":
         raise ValidationError("This name is has already been taken.", code="unique")
