d40371c92d375ed158b30efb45544846951ed4c2
Marcus Caisey <marcus@teckna.com>
2020-06-11 19:53:33 +0100

    Build sass with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,37 @@
 const path = require("path");
-const srcPath = "./js/";
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: "production",
   entry: {
-    base: srcPath + "base.js",
-    home: srcPath + "home.js",
+    "js/base": "./js/base.js",
+    "js/home": "./js/home.js",
+    "css/styles": "./sass/styles.scss",
   },
   output: {
-    path: path.resolve(__dirname, "static/js"),
+    path: path.resolve(__dirname, "static"),
     filename: "[name].js",
   },
+  plugins: [new MiniCssExtractPlugin()],
   module: {
     rules: [
       {
         test: /\.js$/,
-        include: [
-          path.resolve(__dirname, srcPath),
-        ],
+        include: path.resolve(__dirname, "./js"),
         loader: "babel-loader",
         options: {
-          plugins: [
-            "@babel/plugin-proposal-class-properties",
-          ],
+          plugins: ["@babel/plugin-proposal-class-properties"],
           presets: [
             [
               "@babel/preset-env",
-              {
-                targets: "defaults",
-                useBuiltIns: "usage",
-                corejs: 3,
-              },
+              {targets: "defaults", useBuiltIns: "usage", corejs: 3},
             ],
           ],
         },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
