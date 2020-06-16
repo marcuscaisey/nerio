01fb843860baa804160ea1f2d7608be01fb7be87
Marcus Caisey <marcus@teckna.com>
2020-06-16 21:05:40 +0100

    Add deployment config using docker and nginx

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM python:3.8.2-slim-buster
+
+ENV DATABASE_URL sqlite:////srv/nerio/db.sqlite3
+ENV STATIC_ROOT /srv/nerio/static
+ENV ALLOWED_HOSTS nerio.co.uk
+
+RUN mkdir -p /srv/nerio
+
+WORKDIR /usr/src/nerio
+
+COPY requirements/main.txt requirements/main.txt
+
+RUN pip install -r requirements/main.txt
+
+COPY . .
+
+CMD ["gunicorn", "nerio.wsgi"]
---
+++ init-letsencrypt.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo 'Error: docker-compose is not installed.' >&2
+  exit 1
+fi
+
+domains=(nerio.co.uk)
+rsa_key_size=4096
+lets_encrypt_path="/srv/nerio/letsencrypt"
+email="" # Adding a valid address is strongly recommended
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+
+if [ ! -e "$lets_encrypt_path/options-ssl-nginx.conf" ] || [ ! -e "$lets_encrypt_path/ssl-dhparams.pem" ]; then
+    echo "### Downloading recommended TLS parameters ..."
+    mkdir -p "$lets_encrypt_path"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$lets_encrypt_path/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$lets_encrypt_path/ssl-dhparams.pem"
+  echo
+fi
+
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$lets_encrypt_path/live/$domains"
+docker-compose run --rm --entrypoint "\
+  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+echo
+
+
+echo "### Starting nginx ..."
+docker-compose up --force-recreate -d nginx
+echo
+
+echo "### Deleting dummy certificate for $domains ..."
+docker-compose run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domains && \
+  rm -Rf /etc/letsencrypt/archive/$domains && \
+  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
+echo
+
+
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+#Join $domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+docker-compose run --rm --entrypoint "\
+  certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+echo
+
+echo "### Reloading nginx ..."
+docker-compose exec nginx nginx -s reload
