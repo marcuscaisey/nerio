d9dd51885165fd1f95393dd6e277107e2614de32
Marcus Caisey <marcus@teckna.com>
2020-06-22 11:10:53 +0100

    Return None if the request context doesn't have a messages key

--- templatetags/base.py
+++ templatetags/base.py
@@ -9,7 +9,11 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def messages_json(context):
     """Return the messages passed to the template as JSON."""
-    messages = context["messages"]
+    try:
+        messages = context["messages"]
+    except KeyError:
+        return
+
     messages_dict = [
         {"message": m.message, "level": m.level, "tags": m.tags, "extra_tags": m.extra_tags, "level_tag": m.level_tag}
         for m in messages
